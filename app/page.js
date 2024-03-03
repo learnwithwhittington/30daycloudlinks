427235f6c3230031fa73f3219b682a890f9a553e
Whittington <whittingtonsoftware@gmail.com>
2024-03-03 02:00:03 -0500

    hotfix for mobile styles

--- app/page.js
+++ app/page.js
@@ -9,16 +9,11 @@ export default function Home() {
   const links = USER_LINKS.filter((user) => !user.isSample);
   return (
     <main className={styles.main}>
-      <div className="p-5 mb-5 bg-light rounded text-center">
+      <div className={styles.card}>
         <h1>Cozy Cloud Crew 30-Day Cloud Challenge</h1>
-        <p>
-          This is a collection of blog posts from the Cozy Cloud Crew 30-Day Cloud Challenge.
-        </p>
+        <p>This is a collection of blog posts from the Cozy Cloud Crew 30-Day Cloud Challenge.</p>
       </div>
-      <div style={{
-        width: "50%",
-        maxWidth: "600px",
-      }}>
+      <div className={styles.userdetails}>
         <h3 className="text-center fw-bold">User Links</h3>
         {links.map((user, index) => { return <UserDetail key={index} user={user} />; })}
       </div>
